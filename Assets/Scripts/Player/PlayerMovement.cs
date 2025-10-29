174f242cd235d08cfa6c0994202b40db3ad7287a
Brandon Tom <magicink@gmail.com>
2025-10-28 17:43:38 -0700

    refactor: update namespace for PlayerMovement class

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Sandbox.Player
+namespace Player
 {
     [DisallowMultipleComponent]
     public sealed class PlayerMovement : MonoBehaviour
